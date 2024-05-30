c1bd72ef674c6a22d475fcb87ba7e95741270c1e
alex <alex.liswandy@hotmail.com>
2024-05-31 00:25:49 +0800

    Added PageHeader component.

---
+++ src/components/pageHeader/PageHeader.tsx
@@ -0,0 +1,31 @@
+import CartImage from '@/assets/images/icons/shopping-cart.svg';
+import UserImage from '@/assets/images/icons/user.svg';
+import ClickableIcon from '@/components/clickableIcon/ClickableIcon';
+import SearchBar from '@/components/searchBar/SearchBar';
+import Navbar from '@/components/navbar/Navbar';
+import styles from './PageHeader.module.scss';
+
+const PageHeader = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerRowContainer}>
+        <h1>actions per minute.</h1>
+
+        <div className={styles.searchBarContainer}>
+          <SearchBar />
+        </div>
+
+        <div className={styles.iconsContainer}>
+          <ClickableIcon imgSrc={UserImage} altText='User' linkDest='/user' />
+          <ClickableIcon imgSrc={CartImage} altText='Cart' linkDest='/cart' />
+        </div>
+      </div>
+
+      <hr className={styles.headerSeparator} />
+
+      <Navbar />
+    </header>
+  );
+};
+
+export default PageHeader;
