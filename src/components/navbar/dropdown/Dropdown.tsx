9c8d1befb99aea9003b5b4cf5d904b52c876df6d
alex <alex.liswandy@hotmail.com>
2024-05-31 00:05:00 +0800

    Added Dropdown component.

---
+++ src/components/navbar/dropdown/Dropdown.tsx
@@ -0,0 +1,28 @@
+import styles from './Dropdown.module.scss';
+import Link from 'next/link';
+
+const Dropdown = ({ labelText, dropdownContent }: DropdownProps) => {
+  return (
+    <div className={styles.dropdownContainer}>
+      <div className={styles.dropdownTextContainer}>
+        <Link href='/'>{labelText}</Link>
+        <span className={styles.arrow} />
+      </div>
+
+      <ul className={styles.dropdownContent}>
+        {dropdownContent.map(([redirectPath, text]) => (
+          <li key={text}>
+            <Link href={redirectPath}>{text}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+interface DropdownProps {
+  labelText: string;
+  dropdownContent: Array<[string, string]>;
+}
+
+export default Dropdown;
