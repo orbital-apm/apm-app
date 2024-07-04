1cfeb8c6718231fd9758c102bc61345d3a8a6f69
alex <alex.liswandy@hotmail.com>
2024-07-04 22:23:35 +0800

    Added redux-persist.

--- src/components/provider/Provider.tsx
+++ src/components/provider/Provider.tsx
@@ -4,13 +4,18 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider as ReduxProvider } from 'react-redux';
 
-import { store } from '@/utils/store';
+import { persistor, store } from '@/utils/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const queryClient = new QueryClient();
 
 const Provider = ({ children }: { children: React.ReactNode }) => (
   <QueryClientProvider client={queryClient}>
-    <ReduxProvider store={store}>{children}</ReduxProvider>
+    <ReduxProvider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </ReduxProvider>
   </QueryClientProvider>
 );
 
--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,14 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import authReducer from '@/slices/authSlice';
 import builderReducer from '@/slices/builderSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
-    builder: builderReducer,
+    auth: persistReducer(persistConfig, authReducer),
+    builder: persistReducer(persistConfig, builderReducer),
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
