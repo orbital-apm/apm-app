a2e4f9d160e8a098fa3cecc561f2d09435f54356
alex <alex.liswandy@hotmail.com>
2024-07-07 18:49:56 +0800

    PartCard component.

---
+++ src/app/parts/[partKey]/page.tsx
@@ -0,0 +1,11 @@
+import PartComponent from '@/components/parts/part/Part';
+
+const Part = ({ params } : { params: { partKey: string } }) => {
+  return (
+    <main>
+      <PartComponent />
+    </main>
+  );
+}
+
+export default Part;
