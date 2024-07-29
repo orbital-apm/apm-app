099d1158ba0b72a2c7d0f8e0eaae8a56bcbced41
alex <alex.liswandy@hotmail.com>
2024-07-30 00:02:29 +0800

    Select UI component

---
+++ src/components/ui/form/select/Select.tsx
@@ -0,0 +1,19 @@
+import styles from './Select.module.scss';
+
+const Select = ({ id, required, options }: SelectProps) => {
+  return (
+    <select id={id} required={required} className={styles.select}>
+      {options.map(([name, value]) => (
+        <option key={value} value={value}>{name}</option>
+      ))}
+    </select>
+  );
+};
+
+interface SelectProps {
+  id: string;
+  required: boolean;
+  options: [string, number][];
+}
+
+export default Select;
