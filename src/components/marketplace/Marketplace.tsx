4161991bae776e408deedbbf13ea1733c9fcb6dd
alex <alex.liswandy@hotmail.com>
2024-07-30 22:31:26 +0800

    Get listings API integration

--- src/components/marketplace/Marketplace.tsx
+++ src/components/marketplace/Marketplace.tsx
@@ -1,16 +1,26 @@
 'use client';
 
 import Image from 'next/image';
+import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 import styles from './Marketplace.module.scss';
 import ArrowDown from '@/assets/images/icons/arrow-down.svg';
 import RowCard from '@/components/marketplace/rowCard/RowCard';
 import { useAppSelector } from '@/hooks/useAppSelector';
 import Button from '@/components/ui/form/button/Button';
-import Link from 'next/link';
 
 const Marketplace = () => {
   const isAuthenticated = useAppSelector(state => state.auth.isAuthenticated);
+
+  const { data } = useQuery<ListingsResponse>({
+    queryKey: ['listings'],
+    queryFn: async () => {
+      const response = await axios.get(`${process.env.NEXT_PUBLIC_APM_SERVICE_BASE_URL}/v1/listings`);
+      return response.data;
+    }
+  });
 
   return (
     <div className={styles.marketplaceContainer}>
@@ -31,11 +41,35 @@ const Marketplace = () => {
       )}
 
       <div className={styles.listingsContainer}>
-        <RowCard name='hi' description='bye' linkDest='/' price={2.125} />
-        <RowCard name='hi' description='bye' linkDest='/' price={2.125} />
+        {data?.items.map(listing => (
+          <RowCard
+            key={listing.id}
+            name={listing.title}
+            description={listing.description}
+            linkDest='/marketplace'
+            price={listing.price}
+          />
+        ))}
       </div>
     </div>
   );
 };
+
+interface Listing {
+  id: string;
+  title: string;
+  description: string;
+  condition: number;
+  part_type: string[];
+  price: number;
+}
+
+interface ListingsResponse {
+  items: Listing[];
+  total: number;
+  page: number;
+  size: number;
+  pages: number;
+}
 
 export default Marketplace;
