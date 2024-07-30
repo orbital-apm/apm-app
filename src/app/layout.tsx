2e1458443120d0a627781de17ba4f1c69575a315
Phone Mon <monluphone@gmail.com>
2024-07-30 22:46:04 +0800

    1st Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PageHeader from '@/components/pageHeader/PageHeader';
 import Provider from '@/components/provider/Provider';
+import Footer from '@/components/footer/Footer';
 import '../styles/main.scss';
 
 export default function RootLayout({
@@ -20,6 +21,7 @@ export default function RootLayout({
           {children}
         </Provider>
       </body>
+      <Footer/>
     </html>
   );
 }
